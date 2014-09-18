22a49b1c5e17d36a085923e7617a9fac7d7cdfb8
Suhayb El Wardany <me@suw.me>
2014-09-18 20:05:03 +0300

    update copyright headers
    Signed-off-by: Suhayb El Wardany <me@suw.me>

--- src/Cartalyst/NestedSets/Workers/IlluminateWorker.php
+++ src/Cartalyst/NestedSets/Workers/IlluminateWorker.php
@@ -4,25 +4,24 @@
  *
  * NOTICE OF LICENSE
  *
- * Licensed under the 3-clause BSD License.
+ * Licensed under the Cartalyst PSL License.
  *
- * This source file is subject to the 3-clause BSD License that is
- * bundled with this package in the LICENSE file.  It is also available at
- * the following URL: http://www.opensource.org/licenses/BSD-3-Clause
+ * This source file is subject to the Cartalyst PSL License that is
+ * bundled with this package in the license.txt file.
  *
  * @package    Nested Sets
- * @version    2.0.0
+ * @version    2.0.2
  * @author     Cartalyst LLC
- * @license    BSD License (3-clause)
- * @copyright  (c) 2011 - 2013, Cartalyst LLC
+ * @license    Cartalyst PSL
+ * @copyright  (c) 2011-2014, Cartalyst LLC
  * @link       http://cartalyst.com
  */
 
 use Cartalyst\NestedSets\Nodes\NodeInterface;
 use Closure;
 use Illuminate\Database\Connection;
-use Illuminate\Database\Query\Expression;
 use Illuminate\Database\Query\Builder as QueryBuilder;
+use Illuminate\Database\Query\Expression;
 
 /**
  * This class does the MPTT magic which powers nested-sets.
